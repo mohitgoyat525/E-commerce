772b89144808f77f91e112b06d87f17124feaf31
mohit <montyits71@gmail.com>
2025-03-18 15:50:01 +0530

    add-meta-img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Brands from "@/components/Brands";
+import BrowseStyle from "@/components/BrowseStyle";
 import Hero from "@/components/Hero";
 import NewArrivals from "@/components/NewArrivals";
+import TopSelling from "@/components/TopSelling";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,7 +10,9 @@ export default function Home() {
     <>
       <Hero />
       <Brands />
-      <NewArrivals/>
+      <NewArrivals />
+      <TopSelling />
+      <BrowseStyle/>
     </>
   );
 }
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -20,29 +20,61 @@ export const BRANDS_LIST = [
   "/assets/images/webp/versace-logo.webp","/assets/images/webp/zara-logo.webp","/assets/images/webp/gucchi-logo.webp","/assets/images/webp/prada-logo.webp","/assets/images/webp/calvin-klein-logo.webp"
 ]
 
-export const NEW_ARRIVALS_LIST= [
+export const NEW_ARRIVALS_LIST = [
   {
     image: "/assets/images/webp/t-shirt-img-one.webp",
     tittle: "T-SHIRT WITH TAPE DETAILS",
+    rating: "/assets/images/svg/rating-star-4.5-img.svg",
     price: "$120",
   },
   {
     image: "/assets/images/webp/skinny-jeans-img-one.webp",
     tittle: "SKINNY FIT JEANS",
-    price: "$120",
+    rating: "/assets/images/svg/rating-star-3.5-img.svg",
+    price: "$240",
     cancelPrice: "$260",
     discount: "-20%",
   },
   {
     image: "/assets/images/webp/checked-shirt-img-one.webp",
     tittle: "CHECKERED SHIRT",
-    price: "$120",
+    rating: "/assets/images/svg/rating-star-4.5-img.svg",
+    price: "$180",
   },
   {
     image: "/assets/images/webp/seleve-t-shirt-img-.webp",
     tittle: "SLEEVE STRIPED T-SHIRT",
+    rating: "/assets/images/svg/rating-star-4.5-img.svg",
     price: "$130",
     cancelPrice: "$160",
     discount: "-30%",
+  },
+];
+export const TOP_SELLING_LIST = [
+  {
+    image: "/assets/images/webp/verticale-shirt-img.webp",
+    tittle: "VERTICAL STRIPED SHIRT",
+    rating: "/assets/images/svg/rating-star-5-img.svg",
+    price: "$212",
+    cancelPrice: "$232",
+    discount: "-20%",
+  },
+  {
+    image: "/assets/images/webp/orange-t-shirt-img.webp",
+    tittle: "COURAGE GRAPHIC T-SHIRT",
+    rating: "/assets/images/svg/rating-star-4-img.svg",
+    price: "$145",
+  },
+  {
+    image: "/assets/images/webp/loose-fit-img.webp",
+    tittle: "LOOSE FIT BERMUDA SHORTS",
+    rating: "/assets/images/svg/rating-star-3-img.svg",
+    price: "$80",
+  },
+  {
+    image: "/assets/images/webp/faded-pant-img.webp",
+    tittle: "FADED SKINNY JEANS",
+    rating: "/assets/images/svg/rating-star-4.5-img.svg",
+    price: "$210",
   },
 ];
