84b3846e46a474d4347221a41607c06bdaaaf432
mohit <montyits71@gmail.com>
2025-03-19 16:34:36 +0530

    add-changes

--- src/components/review/ReviewHero.tsx
+++ src/components/review/ReviewHero.tsx
@@ -37,96 +37,123 @@ const ReviewHero: React.FC = () => {
   return (
     <>
       <Header />
-      <div className="w-full max-w-[1240px] bg-[#0000001A] mx-auto my-6"></div>
-      <div className="flex items-center gap-2.5 max-w-[1240px] w-full mx-auto">
-        <p className="text-[#00000099]">Home</p>
-        <NextMoveIcon />
-        <p className="text-[#00000099]">Shop</p>
-        <NextMoveIcon />
-        <p className="text-[#00000099]">Men</p>
-        <NextMoveIcon />
-        <p className="text-[#00000099]">T-Shirts</p>
-      </div>
-      <div className="flex items-center w-full max-w-[1240px] mx-auto gap-6 mt-6">
-        <div className="flex items-center flex-col gap-[14px]">
-          <Image src={product.image} alt="product" width={152} height={167} className=" w-full max-w-[152px] h-[167px]"/>
-          <Image src='/assets/images/webp/seleve-t-shirt-img-.webp' alt="product" width={152} height={167} className=" w-full max-w-[152px] h-[167px]"/>
-          <Image src='/assets/images/webp/skinny-jeans-img-one.webp' alt="product" width={152} height={167} className=" w-full max-w-[152px] h-[167px]"/>
+      <div className="px-4">
+        <div className="w-full max-w-[1240px] bg-[#0000001A] mx-auto my-6"></div>
+        <div className="flex items-center gap-2.5 max-w-[1240px] w-full mx-auto">
+          <p className="text-[#00000099]">Home</p>
+          <NextMoveIcon />
+          <p className="text-[#00000099]">Shop</p>
+          <NextMoveIcon />
+          <p className="text-[#00000099]">Men</p>
+          <NextMoveIcon />
+          <p className="text-[#00000099]">T-Shirts</p>
         </div>
-        <Image
-          src={product.image}
-          alt={product.tittle}
-          width={444}
-          height={530}
-          className="w-full max-w-[444px] h-[530px]"
-        />
-        <div className="w-full max-w-[600px]">
-          <h1 className="text-2xl font-bold">{product.tittle}</h1>
-          <Image src={product.rating} alt="rating" width={150} height={19} />
-          <h2 className="text-xl font-bold">{product.price}</h2>
-          <p className="text-[#00000099] font-normal text-base">
-            This graphic t-shirt which is perfect for any occasion. Crafted from
-            a soft and breathable fabric, it offers superior comfort and style.
-          </p>
-          <div className="w-full max-w-[590px] border border-solid border-[#0000001A] my-3"></div>
-
-          {/* Color Selection */}
-          <p>Select Color</p>
-          <div className="flex gap-4">
-            {["#4F4631", "#314F4A", "#31344F"].map((color) => (
-              <div
-                key={color}
-                className={`size-[37px] rounded-full flex items-center justify-center cursor-pointer ${
-                  selectedColor === color ? "border-2 border-black" : ""
-                }`}
-                style={{ backgroundColor: color }}
-                onClick={() => setSelectedColor(color)}
-              >
-                {selectedColor === color && <CheckIcon />}
-              </div>
-            ))}
-          </div>
-
-          <div className="w-full max-w-[590px] border border-solid border-[#0000001A] my-3"></div>
-          {/* Size Selection */}
-          <p>Select Size</p>
-          <div className="flex gap-4">
-            {["Small", "Medium", "Large", "X-Large"].map((size) => (
-              <button
-                key={size}
-                className={`px-6 py-3 border rounded-full cursor-pointer  ${
-                  selectedSize === size ? "bg-black text-white" : "bg-gray-200"
-                }`}
-                onClick={() => setSelectedSize(size)}
-              >
-                {size}
-              </button>
-            ))}
-          </div>
-
-          <div className="w-full max-w-[590px] border border-solid border-[#0000001A] my-3"></div>
-
-          {/* Quantity Selector */}
-          <div className="flex items-center gap-4">
-            <div className="flex items-center justify-between w-full max-w-[170px] rounded-full bg-[#F0F0F0] p-2">
-              <button
-                className="px-3"
-                onClick={() => setQuantity((prev) => (prev > 1 ? prev - 1 : 1))}
-              >
-                -
-              </button>
-              <p>{quantity}</p>
-              <button
-                className="px-3"
-                onClick={() => setQuantity((prev) => prev + 1)}
-              >
-                +
-              </button>
+        <div className="flex items-center w-full max-w-[1240px] max-lg:flex-wrap mx-auto gap-6 mt-6">
+          <div className="flex items-center gap-4 flex-wrap-reverse">
+            <div className="max-lg:flex  gap-[14px]">
+              <Image
+                src={product.image}
+                alt="product"
+                width={152}
+                height={167}
+                className=" w-full max-w-[152px] h-[167px] max-sm:max-w-[111px] max-sm:h-[106px]"
+              />
+              <Image
+                src="/assets/images/webp/seleve-t-shirt-img-.webp"
+                alt="product"
+                width={152}
+                height={167}
+                className=" w-full max-w-[152px] h-[167px] max-sm:max-w-[111px] max-sm:h-[106px]"
+              />
+              <Image
+                src="/assets/images/webp/skinny-jeans-img-one.webp"
+                alt="product"
+                width={152}
+                height={167}
+                className=" w-full max-w-[152px] h-[167px] max-sm:max-w-[111px] max-sm:h-[106px]"
+              />
             </div>
+            <Image
+              src={product.image}
+              alt={product.tittle}
+              width={444}
+              height={530}
+              className="w-full max-w-[444px] h-[530px] max-sm:max-w-[358px] max-sm:h-[290px]"
+            />
           </div>
-          <button className="w-full bg-black text-white py-3 mt-4 rounded-full">
-            Add to Cart
-          </button>
+          <div className="w-full max-w-[600px]">
+            <h1 className="text-2xl font-bold">{product.tittle}</h1>
+            <Image src={product.rating} alt="rating" width={150} height={19} />
+            <h2 className="text-xl font-bold">{product.price}</h2>
+            <p className="text-[#00000099] font-normal text-base">
+              This graphic t-shirt which is perfect for any occasion. Crafted
+              from a soft and breathable fabric, it offers superior comfort and
+              style.
+            </p>
+            <div className="w-full max-w-[590px] border border-solid border-[#0000001A] my-3"></div>
+
+            {/* Color Selection */}
+            <p>Select Color</p>
+            <div className="flex gap-4">
+              {["#4F4631", "#314F4A", "#31344F"].map((color) => (
+                <div
+                  key={color}
+                  className={`size-[37px] rounded-full flex items-center justify-center cursor-pointer ${
+                    selectedColor === color ? "border-2 border-black" : ""
+                  }`}
+                  style={{ backgroundColor: color }}
+                  onClick={() => setSelectedColor(color)}
+                >
+                  {selectedColor === color && <CheckIcon />}
+                </div>
+              ))}
+            </div>
+
+            <div className="w-full max-w-[590px] border border-solid border-[#0000001A] my-3"></div>
+            {/* Size Selection */}
+            <p>Select Size</p>
+            <div className="flex gap-4">
+              {["Small", "Medium", "Large", "X-Large"].map((size) => (
+                <button
+                  key={size}
+                  className={`px-6 py-3  max-sm:px-3 max-sm:py-2 border rounded-full cursor-pointer  ${
+                    selectedSize === size
+                      ? "bg-black text-white"
+                      : "bg-gray-200"
+                  }`}
+                  onClick={() => setSelectedSize(size)}
+                >
+                  {size}
+                </button>
+              ))}
+            </div>
+
+            <div className="w-full max-w-[590px] border border-solid border-[#0000001A] my-3"></div>
+
+            {/* Quantity Selector */}
+            <div className="flex items-center gap-4">
+              <div className="flex items-center justify-between w-full max-w-[170px] rounded-full bg-[#F0F0F0] p-2">
+                <button
+                  className="px-3"
+                  onClick={() =>
+                    setQuantity((prev) => (prev > 1 ? prev - 1 : 1))
+                  }
+                >
+                  -
+                </button>
+                <p>{quantity}</p>
+                <button
+                  className="px-3"
+                  onClick={() => setQuantity((prev) => prev + 1)}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <button className="w-full bg-black text-white py-3 mt-4 rounded-full">
+              Add to Cart
+            </button>
+          </div>
         </div>
       </div>
     </>
